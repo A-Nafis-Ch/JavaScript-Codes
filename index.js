83ba37fc2b1e3319764e0ea22c5468474a6d832d
Abdullah Nafis Chowdhury <abdullahnafis256@gmail.com>
2024-12-13 21:20:46 +0600

    Files updated with new codes

---
+++ index.js
@@ -0,0 +1,53 @@
+// document.getElementById('heading1').innerHTML="Heading1 is changed!"; //getElementById
+// document.getElementsByClassName('class1')[0].innerHTML = "Para is changed!"; //when you add Element(s) then it becomes array. getElementsbyClassName.
+
+       //QUERYSELECTOR
+//document.querySelector("#pid").innerHTML = "paragraph is changed";
+//document.querySelector('p').innerHTML = "Para2 is changed";
+
+// document.querySelector('a').innerHTML="Home link";
+// document.querySelector('li a').innerHTML="Tutorial link name is changed";
+// document.querySelector('.my-div a').innerHTML = "Contact link name is changed";
+
+                //QUERYSELECTORALL
+
+// document.querySelectorAll('p')[0].innerHTML = "para is changed!";  
+
+                //EVENTLISTENER/ONCLICK
+
+// function myMessage(){
+
+//     alert("You have clicked a button")
+// }    
+
+// var c1 = document.querySelector('#pid');
+
+// function click1(){
+
+
+//     c1.innerHTML = "You have clicked button 1";
+
+
+// }
+
+// function click2(){
+
+    
+//     c1.innerHTML = "You have clicked button 2";
+
+
+// }
+
+var x = document.querySelector('#imgid');
+
+function img1(){
+
+    x.src = "eagleye.jpg";
+}
+function img2(){
+
+    x.src = "snake.jpg";
+}
+
+
+
